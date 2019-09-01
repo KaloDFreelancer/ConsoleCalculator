40f7a7b05e8e16203bb368a476baea6187994d10
KaloDFreelancer <dm.kaloyan@gmail.com>
2019-09-01 19:15:39 +0300

    Define colors and pick languages
    New requirements.

--- CalculatorLogic/Calculator.cs
+++ CalculatorLogic/Calculator.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using System.Linq;
 
 namespace CalculatorLogic
 {
     public class Calculator
     {
-        public ConsoleColor color = ConsoleColor.White;
-
+        public ConsoleColor color;
+        public void Color(string colorName)
+        {
+            string[] result = Enum.GetNames(typeof(ConsoleColor));
+            colorName= StringExtensions.FirstCharToUpper(colorName);
+            if (result.Contains(colorName))
+            {
+               Enum.TryParse(colorName, out ConsoleColor consoleColor);
+                this.color = consoleColor;
+            }
+        }
         public double Sum(double a, double b)
         {
             var x = a + b;
@@ -36,6 +46,19 @@ namespace CalculatorLogic
             Console.ForegroundColor = color;
             Console.WriteLine($"The result is: {x}");
             return x;
+        }
+
+    }
+    public static class StringExtensions
+    {
+        public static string FirstCharToUpper(this string input)
+        {
+            switch (input)
+            {
+                case null: throw new ArgumentNullException(nameof(input));
+                case "": throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input));
+                default: return input.First().ToString().ToUpper() + input.Substring(1);
+            }
         }
     }
 }
---
+++ ConsoleCalculator/Languages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleCalculator
+{
+    //
+    // Summary:
+    //     Here you can see which languages are available.
+    public enum Languages
+    {
+        English,
+        Bulgarian
+
+    }
+}
