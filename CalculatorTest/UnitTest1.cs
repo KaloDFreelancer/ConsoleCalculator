e08201b023c63e2d8e8b31c904fdf6a92cdc8ca7
KaloDFreelancer <dm.kaloyan@gmail.com>
2019-09-01 18:23:34 +0300

    Added some Unit tests
    Used NUnit to see if everything is working as planned

--- CalculatorTest/UnitTest1.cs
+++ CalculatorTest/UnitTest1.cs
@@ -1,11 +1,42 @@
 using CalculatorLogic;
-using Xunit;
+using NUnit.Framework;
 
 namespace CalculatorTest
 {
-    
+    [TestFixture]
     public class UnitTest1
     {
-        
+        Calculator calculator;
+        [SetUp]
+        public void Setup()
+        {
+            this.calculator = new Calculator();
+        }
+        [Test]
+        public void TestSum()
+        {
+            var sum = calculator.Sum(100, 200);
+            Assert.AreEqual(300, sum);
+        }
+        [Test]
+        public void TestSubstract()
+        {
+            var sum = calculator.Subtract(100, 200);
+            Assert.AreEqual(-100, sum);
+        }
+        [Test]
+        public void TestMultiply()
+        {
+            var sum = calculator.Multiply(100, 200);
+            Assert.AreEqual(20000, sum);
+        }
+        [Test]
+        public void TestDivide()
+        {
+            var sum = calculator.Divide(100, 200);
+            Assert.AreEqual(0.5, sum);
+        }
+
+
     }
 }
